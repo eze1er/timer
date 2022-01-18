1cb2989c9b4644743c58210b6dad8fd315a79e3f
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-01-18 18:14:48 -0500

    Update this by the new v

--- timer1.js
+++ timer1.js
@@ -1,21 +1,19 @@
 'use strict';
 const timerSound = process.argv.slice(2);
-let timer = 0
+let timer = 1000;
 for (let i = 0; i < timerSound.length; i++) {
   setTimeout(() => {
     // the timer sound
     process.stdout.write('\x07');
-    // increment my timer
+    console.log('.');
     if (i === timerSound.length - 1) {
-      timer += (1000 * timerSound[i]) ;
       timer += ('\n');
-    } else {
-      timer += (1100 * timerSound[i]) ;
     }
-     
   }, timer);
-
+  // increment my timer outside of setTimeout
+  timer += (1000 * timerSound[i]);
 }
 
 // for test you should run node with number for timer.
+
 
